98270a8b2f9f31791d10ecf309d2b00c6a91e598
Thanthip Fawn Orbesen <torbesen@cnm.edu>
2022-03-31 20:53:52 -0600

    Adding index and script files.

---
+++ script.js
@@ -0,0 +1,85 @@
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit",(event) => {
+        handleItemForm(event, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault) {  //prevent JavaScript from automatically submitting the form
+        event.preventDefault();
+    }
+
+    addItemtoShoppingList();
+    formRef.reset();
+    return false;
+}
+
+function addItemtoShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+    let id = getRandomeInt(0,10000000);
+
+    // Creates list item html and appends to page.
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+    console.log("Item HTML: ", itemHtml);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml)
+
+    setDeleteButtonEvent(id);
+}
+
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button" + id);
+    deleteButton.addEventListener("click", () => {
+        removeListItem(id);
+    })
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}">
+                ${itemName} - ${itemAmount}
+                <button id="button${id}">Delete Item</button>
+                </li>`;
+}
+
+function removeListItem(id) {
+    let listItem = document.getElementById("item" + id);
+    listItem.parentNode.removeChild(listItem);
+}
+
+function getRandomeInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/*function handleItemForm (event, formRef) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }
+
+    addItemToShoppingList();
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+    let itemHtml = createListItemHtml();
+    console.log("Item HTML: ", itemHtml);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+}
+
+function createListItemHtml() {
+    return '<li>Item Name - Amount </li>';
+}
+
+ */
